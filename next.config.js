c4f0935812dc1c650026d7e31574b3e864784231
nataliea772 <natalie22ad@gmail.com>
2025-05-24 19:46:45 +0300

    fix: update server and Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
   images: {
     unoptimized: true,
   },
 }
 
-module.exports = nextConfig
+module.exports = nextConfig 
