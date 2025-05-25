040762b0db508d16b4faa46b56e4af94f14b20d0
nataliea772 <natalie22ad@gmail.com>
2025-05-25 15:54:04 +0300

    updated title font

--- components/Quiz.tsx
+++ components/Quiz.tsx
@@ -105,7 +105,7 @@ export default function Quiz() {
 
   return (
     <div className={styles.quiz}>
-      <h1 className={styles.title}>Quiz Bot</h1>
+      <h1 className={styles.title}>QUIZLY</h1>
 
       <div className={styles.inputSection}>
         <input
